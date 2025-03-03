a5c123ad6f5a2885ac4ce7517fa06eaf89f3caae
PaoMultiverse <baot21278@gmail.com>
2025-03-03 10:29:06 +0700

    test 4

--- server/model/ShoppingCartSchema.js
+++ server/model/ShoppingCartSchema.js
@@ -1,22 +1,25 @@
-import mongoose, { Schema } from 'mongoose';
-import { DB_SCHEMA } from '../configs/Constants.js';
-const ObjectId = mongoose.Types.ObjectId
+import mongoose, { Schema } from "mongoose";
+import { DB_SCHEMA } from "../configs/Constants.js";
+const ObjectId = mongoose.Types.ObjectId;
 
-const ShoppingCartSchema = new Schema({
-    user: {type: ObjectId , ref: DB_SCHEMA.USER, required: true},
+const ShoppingCartSchema = new Schema(
+  {
+    user: { type: ObjectId, ref: DB_SCHEMA.USER, required: true },
     items: [
-        {
-            product: { type: ObjectId, ref: DB_SCHEMA.PRODUCT, required: true },
-            quantity: { type: Number, required: false, min: 1 },
-            priceTotal: { type: Number, required: false }
-        }
+      {
+        product: { type: ObjectId, ref: DB_SCHEMA.PRODUCT, required: true },
+        quantity: { type: Number, required: false, min: 1 },
+        priceTotal: { type: Number, required: false },
+      },
     ],
     count: {
-        type:Number,
-        max: 99,
-    }
-},{ timestamps: true });
+      type: Number,
+      max: 99,
+    },
+  },
+  { timestamps: true }
+);
 
-const ShoppingCart = mongoose.model('ShoppingCart', ShoppingCartSchema);
+const ShoppingCart = mongoose.model(DB_SCHEMA.SHOPINGCART, ShoppingCartSchema);
 
 export default ShoppingCart;
